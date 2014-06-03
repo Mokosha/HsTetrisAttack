3f54f8a9337d3925e26ac46df8de75a218c6301f
Pavel Krajcevski <pavel@cs.unc.edu>
2014-06-02 20:30:08 -0400

    Add initial board generation

--- Main.hs
+++ Main.hs
@@ -9,7 +9,7 @@ import Linear.Vector
 import Linear.V2
 import Linear.V3
 
-import Control.Wire
+import Control.Wire hiding ((.))
 import Control.Monad.RWS.Strict hiding (when)
 
 import System.FilePath
@@ -17,7 +17,9 @@ import System.FilePath
 import Paths_TetrisAttack
 import TetrisAttack.Constants
 
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as V
+import qualified Data.Map as Map
+import Data.Word
 
 --------------------------------------------------------------------------------
 
@@ -29,10 +31,7 @@ type Cursor = (Location, Bool)
 
 data TileColor = Red | Green | Blue | Yellow | Purple
                deriving (Eq, Show, Ord, Read)
-data TileType = Stationary TileColor
-                -- Tile is swapping with another, this contains what the tile will be
-                -- once it's done swapping
-              | Swapping Tile
+type TileMap = Map.Map TileColor L.RenderObject
 
 mkCursor :: Location -> IO (L.GameWire Float Cursor)
 mkCursor loc' = do
@@ -72,25 +71,40 @@ mkCursor loc' = do
         [GLFW.Key'Up, GLFW.Key'Down, GLFW.Key'Left, GLFW.Key'Right, GLFW.Key'Space]
       return (Right (newloc, L.isKeyPressed GLFW.Key'Space ipt), cursor newloc)
 
-type Tile = (Location, TileType)
+type Tile = (Location, TileColor)
 type TileLogic = L.GameWire Float Tile
 type Board = V.Vector (V.Vector TileLogic)
 
-initBoard :: [Tile]
-initBoard = zipWith (,) ([(x, y) | x <- [1..blocksPerRow], y <- [1..(rowsPerBoard - 10)]])
-               (cycle $ map Stationary [Red, Green, Blue, Yellow, Purple])
+stationary :: TileMap -> TileColor -> Location -> L.GameWire Float Tile
+stationary m color loc =
+  let ro = m Map.! color
+      (V2 trx try) = blockCenter loc
+      xf = L.translate (V3 trx try $ renderDepth RenderLayer'Tiles) $
+           L.nonuniformScale (0.5 *^ (fmap fromIntegral (V3 blockSize blockSize 1))) $
+           L.identity
+  in
+   mkGen_ $ \_ -> censor (L.Render3DAction xf ro :) (return $ Right (loc, color))
+
+initBoard :: TileMap -> Board
+initBoard m =
+  V.generate blocksPerRow $ \col ->
+  V.generate (rowsPerBoard - 10) $ \row ->
+  map (flip (stationary m) (col+1, row+1)) [Red, Green, Blue, Yellow, Purple] !! ((row + col) `mod` 5)
 
 analyzeTiles :: [Tile] -> GameResult
 analyzeTiles tiles
   | any (\((_, y), _) -> y > rowsPerBoard) tiles = GameOver
   | otherwise = Running
 
-mkBoard :: [Tile] -> IO (L.GameWire Cursor [Tile])
-mkBoard tiles' = do
+type ColumnCollection = Either () ([Tile], [TileLogic])
+type RowCollection = Either () ([Tile], [V.Vector TileLogic])
+
+mkBoard :: Board -> IO (L.GameWire Cursor [Tile])
+mkBoard board' = do
 --  (Just bgTex) <- getDataFileName ("background" <.> "png") >>= L.loadTextureFromPNG
-  bgTex <- L.createSolidTexture (67, 128, 67, 255)
+  bgTex <- L.createSolidTexture (10, 20, 10, 255)
   bg <- L.createRenderObject L.quad (L.createTexturedMaterial bgTex)
-  return (boardLogic tiles' >>> (boardRender bg))
+  return (boardLogic board' >>> (boardRender bg))
   where
     boardRender :: L.RenderObject -> L.GameWire [Tile] [Tile]
     boardRender ro = mkGen_ $ \tiles -> do
@@ -100,19 +114,56 @@ mkBoard tiles' = do
       censor (L.Render3DAction xf ro :) $
         return (Right tiles)
 
-    boardLogic :: [Tile] -> L.GameWire Cursor [Tile]
-    boardLogic tiles = mkGenN $ \(loc, swap) -> do
-      return (Right tiles, boardLogic tiles)
+    boardLogic :: Board -> L.GameWire Cursor [Tile]
+    boardLogic board = let
+
+      collectCol :: L.TimeStep -> ColumnCollection -> TileLogic -> L.GameMonad ColumnCollection
+      collectCol _ (Left _) _ = return $ Left ()
+      collectCol timestep (Right (ts, wires)) wire = do
+        (newTile, newWire) <- stepWire wire timestep (Right 0)
+        case newTile of
+          Right tile -> return $ Right (tile : ts, newWire : wires)
+          Left _ -> return $ Left ()
+
+      collectRow :: L.TimeStep -> RowCollection -> (V.Vector TileLogic) -> L.GameMonad RowCollection
+      collectRow _ (Left _) _ = return $ Left ()
+      collectRow timestep (Right (ts, logic)) col = do
+        result <- V.foldM (collectCol timestep) (Right ([], [])) col
+        case result of
+          Right (tiles, wires) -> return $ Right (tiles ++ ts, V.fromList (reverse wires) : logic)
+          Left _ -> return $ Left ()
+     in
+      mkGen $ \timestep (loc, swap) -> do
+        result <- V.foldM (collectRow timestep) (Right ([], [])) board
+        case result of
+          Right (tiles, nextBoard) -> return $ (Right tiles, boardLogic (V.fromList $ reverse nextBoard))
+          Left _ -> return (Left (), boardLogic board)
 
 data GameResult = GameOver
                 | Running
                 deriving (Eq, Show, Ord, Read)
 
 game :: IO (L.GameWire GameResult GameResult)
-game = do
-  board <- mkBoard initBoard
-  cursor <- mkCursor boardCenter
-  return $ when (== Running) >>> (pure 0) >>> cursor >>> board >>> (arr analyzeTiles)
+game = let
+  tilecolors = [Red, Green, Blue, Yellow, Purple]
+
+  tile2rgb :: TileColor -> (Word8, Word8, Word8, Word8)
+  tile2rgb Red = (200, 0, 0, 255)
+  tile2rgb Green = (67, 128, 67, 255)
+  tile2rgb Blue = (0, 0, 200, 255)
+  tile2rgb Yellow = (180, 180, 0, 255)
+  tile2rgb Purple = (0, 180, 180, 255)
+
+  loadColor :: (Word8, Word8, Word8, Word8) -> IO (L.RenderObject)
+  loadColor color = do
+    tex <- L.createSolidTexture color
+    L.createRenderObject L.quad (L.createTexturedMaterial tex)
+  in
+   do
+     ros <- mapM (loadColor . tile2rgb) tilecolors
+     board <- mkBoard $ initBoard (Map.fromList $ zip tilecolors ros)
+     cursor <- mkCursor boardCenter
+     return $ when (== Running) >>> (pure 0) >>> cursor >>> board >>> (arr analyzeTiles)
 
 initGame :: IO (L.Game GameResult)
 initGame = do
